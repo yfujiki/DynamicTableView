08c9eff684a76e284843c1faee07d5b4e704f896
Yuichi Fujiki <yuichi@chaione.com>
2015-10-28 14:35:47 -0500

    Additional commit...

---
+++ DynamicTableView/TableHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  TableHeaderView.swift
+//  DynamicTableView
+//
+//  Created by Yuichi Fujiki on 8/28/15.
+//  Copyright (c) 2015 responsivebytes. All rights reserved.
+//
+
+import UIKit
+
+class TableHeaderView: UIView {
+
+    @IBOutlet weak var addButton: UIButton!
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+}
--- DynamicTableView/ViewController.swift
+++ DynamicTableView/ViewController.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var tableViewHeightConstraint: NSLayoutConstraint!
+
+    var data:[String] = ["Initial data1", "Initial data2"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "identifier")
+
+        tableView.reloadData()
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+
+        tableViewHeightConstraint.constant = tableView.contentSize.height
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +36,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func add(sender: AnyObject) {
+        data.append("Additional data\(data.count + 1)")
 
+        tableView.reloadData()
+
+        tableViewHeightConstraint.constant = tableView.contentSize.height
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("identifier")
+
+        cell?.textLabel?.text = data[indexPath.row]
+
+        return cell!
+    }
 }
 
